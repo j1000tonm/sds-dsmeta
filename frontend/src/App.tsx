8c66854a73e71e17a37053ea0ab61f3f16bc5a91
José Milton <j1000tonm.jmb@gmail.com>
2022-07-13 17:10:15 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
-import NotificationButton from "./components/NotificationButton";
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
 
 function App() {
   return (
     <>
-      <h1>Olá Milton!</h1>
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div class="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   );
 }
